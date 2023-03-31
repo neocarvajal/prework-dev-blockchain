80444a5db633aa89c6bd51b6f473b5a94e950c4b
neocarvajal <neocarvajal@gmail.com>
2023-03-31 02:36:33 -0400

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     goerli:{
-      url: '', // Add your Alchemy App Key
-      accounts: [""] //Add private key from Metamask
+      url: 'https://eth-goerli.g.alchemy.com/v2/zhPMn7BpmRD4s6bcgvY-IVj8bNkC6Jjv',
+      accounts: ["3810c28c1f6dfbe2a6fae2b5c045483f087048946163a81b8db136897729ca19"]
     }
   }
 };
