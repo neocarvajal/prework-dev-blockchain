8635fa4d717d8ad609aded6e720a4daf2042cf90
Erick Carvajal R <neocarvajal@gmail.com>
2023-07-14 01:15:52 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     goerli:{
-      url: 'https://eth-goerli.g.alchemy.com/v2/zhPMn7BpmRD4s6bcgvY-IVj8bNkC6Jjv',
-      accounts: ["7f46104107298c8c04934053249e5a48b1e410a5d8c1f458ee7e65ec00d45103"]
+      url: '',
+      accounts: [""]
     }
   }
 };
